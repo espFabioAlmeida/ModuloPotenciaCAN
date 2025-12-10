de20d0027f9497a3f024b1d4f0eda90938ff41c8
Fábio Almeida <teg.fabio.almeida@gamil.com>
2025-12-10 19:24:51 -0300

    Commit Inicial

---
+++ Core/Inc/global.h
@@ -0,0 +1,18 @@
+/*
+ * global.h
+ *
+ *  Created on: Aug 23, 2023
+ *      Author: User
+ */
+
+#ifndef INC_GLOBAL_H_
+#define INC_GLOBAL_H_
+
+#define VERSAO_FIRMWARE 1
+
+enum BOOL {
+	false,
+	true
+};
+
+#endif /* INC_GLOBAL_H_ */
